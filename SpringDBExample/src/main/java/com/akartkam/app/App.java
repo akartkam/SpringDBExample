58e823225637767deb759cba5dd22a1420f651d6
Artur Akchurin <akartkam@gmail.com>
2014-05-08 22:07:31 +0400

    Signed-off-by: Artur Akchurin <akartkam@gmail.com>

---
+++ SpringDBExample/src/main/java/com/akartkam/app/App.java
@@ -0,0 +1,34 @@
+package com.akartkam.app;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class App {
+	 
+	
+	public static void main(String[] args) throws Exception {
+
+		
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("SpringDBExample.xml");
+     
+		ComboPooledDataSource ds = (ComboPooledDataSource) appContext.getBean("dataSource");
+		//CustomerBo customer = (CustomerBo) appContext.getBean("customerBo");
+		//customer.addCustomer();
+		
+		//customer.addCustomerReturnValue();
+		
+		//customer.addCustomerThrowException();
+		
+		//customer.addCustomerAround("mkyong");
+		
+
+		
+	   // Performer eddie = (Performer) appContext.getBean("eddie");
+	   // eddie.perform();
+	   // ((Contestant)eddie).receiveAward();
+	    
+
+	}
+}
